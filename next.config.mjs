0bdf2cf91d5ff32f5f49076a768260625d4fefab
sarahrubia <sarahrubia22@gmail.com>
2025-08-23 14:36:30 -0300

    config: add configuration files

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,10 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  output: 'export',
+  trailingSlash: true,
+  basePath: process.env.NODE_ENV === 'production' ? '/narrativa-almerinda' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/narrativa-almerinda/' : '',
 }
 
 export default nextConfig
